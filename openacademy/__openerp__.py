85dc5b640e9068c4cd1a19d057cb860ee39745f5
tonatonantzin <tonatonantzin@gmail.com>
2016-01-12 19:19:03 +0000

    [REF] openacademy: Add fata demo.

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -31,7 +31,7 @@
     ],
     # only loaded in demonstration mode
     'demo': [
-        #'demo.xml',
+        'demo/openacademy_course_demo.xml',
     ],
     'installable': True,
     'auto_install': False,
