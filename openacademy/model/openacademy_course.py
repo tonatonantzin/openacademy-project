482b47e51f35a90d27d6774e42cd022a71088090
tonatonantzin <tonatonantzin@gmail.com>
2016-01-14 17:58:12 +0000

    [REF] openacademy: add relation m2o.

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -12,3 +12,7 @@ class Course(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description')
+    responsible_id = fields.Many2one('res.users',
+                                    ondelete='set null', 
+                                    string="Responsible", index=True)
+
