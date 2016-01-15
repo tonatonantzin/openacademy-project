da4f2cfb57e2d62b84bb5467e275e3f0542ba9c3
tonatonantzin <tonatonantzin@gmail.com>
2016-01-15 18:49:04 +0000

    [REF] openacademy: add m2m field session-instructor.

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -28,6 +28,7 @@
     'data': [
         'views/openacademy_course_view.xml',
         'views/openacademy_session_view.xml',
+        'views/partner_view.xml',
         # 'security/ir.model.access.csv',
         #'templates.xml',
     ],
