cf79b473f39d75e05c1e4e3241c06d8f29ca4f03
tonatonantzin <tonatonantzin@gmail.com>
2016-01-18 20:12:34 +0000

    [REF] openacdemy: add color to tree view session.

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -27,3 +27,17 @@ class Course(models.Model):
          "The course title must be unique"),
     ]
 
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
